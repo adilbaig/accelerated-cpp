a7be378934805df71266130afab36c416bf5ee31
adil <adil.baig@aidezigns.com>
2016-11-23 19:09:44 +0530

    Handle

---
+++ ch14/Handle.cpp
@@ -0,0 +1,40 @@
+/*
+ * Handle.cpp
+ *
+ *  Created on: 21-Nov-2016
+ *      Author: adil
+ */
+
+template <class T> class Handle
+{
+    
+public:
+    Handle(): p(0) {}
+    Handle(const Handle& s) : p(0) {
+        if (s.p)
+            p = s.p->clone();
+    }
+    
+    Handle& operator=(const Handle&);
+    ~Handle() { delete p; }
+    
+    Handle(T* t) p(t) {}
+    
+    operator bool() const { return p; }
+    T& operator*() const;
+    T* operator->() const;
+    
+private:
+    T* p;
+}
+
+template <class T>
+Handle<T>& Handle<T>::operator=(const Handle& rhs)
+{
+    if(&rhs != this) {
+        delete p;
+        p = rhs.p ? rhs.p->clone() : 0;
+    }
+    
+    return *this;
+}
